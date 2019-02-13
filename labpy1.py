ad9f596dba6c47ec6ea52478600ab2b320701fe5
ribkatiyoris26 <ribkatiyoris26@gmail.com>
2019-02-13 12:09:59 +0700

    first commit

---
+++ labpy1.py
@@ -0,0 +1,13 @@
+a = int(input("bilanngan pertama: "))
+b = int(input("bilangan kedua: "))
+c = int(input("bilangan ketiga: "))
+
+if a>b and a>c:
+       print(a,"nilai terbesar")
+elif b>a and b>c:
+       print (b,"nilai terbesar")
+elif c>a and c>b:
+         print(c,"nilai terbesar")
+
+else:
+         print("salah")
